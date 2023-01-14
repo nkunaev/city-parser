0201ad58abb4b48e6712108e6e35c2751d57e06d
nikita.kunaev <azero.kunaev@gmail.com>
2023-01-14 13:49:41 +0300

    add funk get_city. It recive city name and return url to it's page

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import requests, re
-
-def searchCity():
-    city_ru = 'Дубна'
-    r = requests.get('https://dom.mingkh.ru/moskovskaya-oblast/#all_cities')
-    var = re.findall(f'<a href="(/moskovskaya-oblast/.{1,15})">{city_ru}</a>', r.text)
-    print(var)
-    for i in var:
-       print(i.strip().replace(' ', ''))
+import requests
+from bs4 import BeautifulSoup
 
 
-searchCity()+
+def get_city(city: str):
+    if type(city) != str:
+        print("Нужно написать название города" + '\n')
+        return 0
+    city = city.lower()
+    city_list = {}
+    regions = requests.get('https://dom.mingkh.ru/moskovskaya-oblast/#all_cities')
+    soup = BeautifulSoup(regions.text, "lxml")
+    data = soup.find("ul", class_="list-unstyled list-columns")
+    for data_city in data.find_all("li"):
+        url = "https://dom.mingkh.ru" + data_city.find("a").get("href")
+        name = data_city.find("a").text.replace("\n", "").lower()
+        city_list[name] = url
+    if city == "search":
+        print("Вот список населенных пунктов:")
+        for city_ in city_list.keys():
+            print(city_.capitalize())
+    elif city in city_list.keys():
+        return city_list[city]
+    else:
+        print("Такого города нет, вот список населенных пунктов:")
+        for city_ in city_list.keys():
+            print(city_.capitalize())
+        return 0
+
+
+print(get_city())
